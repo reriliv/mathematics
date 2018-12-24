6524c81ed4a03811e5904b2c6911de1f13e21d7a
Reriliv <reri_liv@163.com>
2018-12-24 23:57:07 +0800

    first commit

---
+++ src/Lesson1_3.java
@@ -0,0 +1,10 @@
+public class Lesson1_3 {
+	/**
+	 * @Description: 二进制按位“或”的操作
+	 * @paeam num1- 第一个数字， num2- 第二个数字
+	 * @return 二进制按位“或”的结果
+	 * */
+	public static int or(int num1, int num2){
+		return (num1 | num2);
+	}
+}
